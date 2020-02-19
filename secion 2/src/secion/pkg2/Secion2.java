84c9f6884255734f413781e9fe79ff996ac701d4
EdgarVitela <60618629+EdgarVitela@users.noreply.github.com>
2020-02-19 15:49:06 -0600

    Add files via upload

---
+++ secion 2/src/secion/pkg2/Secion2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package secion.pkg2;
+
+import ico.fes.poo.reloj;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Secion2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        reloj wacho= new reloj(12, 37, 44);
+        wacho.incrementarSegundo();
+    }
+    
+}
